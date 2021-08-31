e9fd01c612448803b8104ab4817203c9d752864e
anthow <anthonyenglebert@gmail.com>
2021-08-31 15:28:47 +0200

    /

--- src/components/header.js
+++ src/components/header.js
@@ -19,24 +19,24 @@ import { useStaticQuery, graphql } from "gatsby"
 `) 
 return(
   <header className=" sticky top-0 z-50  font-bold bg-white ">
-<section className="w-10/12 m-auto justify-between  items-center py-2 flex " >
+<section className="w-10/12 m-auto justify-between  items-center py-8 flex " >
   <figure>
   <GatsbyImage image={data.datoCmsLogo.logoSirius.gatsbyImageData} />  </figure> 
     <nav>
       <ul className="flex  text-white space-x-4 ">
-      <li><Link to="/#school"  className=" text-siriusblue hover:text-red-500">Sirius School</Link></li>
-      <li><Link to="/#formations"  className=" text-siriusblue hover:text-red-500">Les formations</Link></li>
-      <li><Link to="/#témoignages"  className=" text-siriusblue hover:text-red-500"> Témoignages</Link></li>
-      <li><Link to="/#inscription"  className=" text-siriusblue hover:text-red-500"> Inscription</Link></li>
-      <li><Link to="/#contact"  className=" text-siriusblue hover:text-red-500"> contact</Link></li>
+      <li><Link to="/#school"  className=" text-siriusorange hover:text-opacity-70">Sirius School</Link></li>
+      <li><Link to="/#formations"  className=" text-siriusorange hover:text-opacity-70">Les formations</Link></li>
+      <li><Link to="/#témoignages"  className=" text-siriusorange hover:text-opacity-70"> Témoignages</Link></li>
+      <li><Link to="/#inscription"  className=" text-siriusorange hover:text-opacity-70"> Inscription</Link></li>
+      <li><Link to="/#contact"  className=" text-siriusorange hover:text-opacity-70"> Contact</Link></li>
 
 
       </ul>
     </nav>
     <nav>
       <ul className="flex space-x-4 ">
-      <li><a src="https://siriushub.be" target="_blank"  className=" text-siriusorange hover:text-red-500">Sirius Hub</a></li>
-      <li><a src="https://www.possibles.org/" target="_blank"  className=" text-siriusorange hover:text-red-500">MDP</a></li>
+      <li><a src="https://siriushub.be" target="_blank"  className=" text-siriusblue hover:text-opacity-70">Sirius Hub</a></li>
+      <li><a src="https://www.possibles.org/" target="_blank"  className=" text-siriusblue   hover:text-opacity-70">MDP</a></li>
       </ul>
     </nav>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,43 +14,49 @@ const IndexPage = ({ data }) => (
  <GatsbyImage image={data.datoCmsHomepage.imageSlider.gatsbyImageData} />
    </figure> 
 
-    <h1 id="school" className="mt-20 text-4xl text-siriusblue font-bold  text-center mb-12">Sirius School</h1>
-<section className="w-10/12 m-auto place-content-center items-center gap-4 auto-cols-fr auto-rows-min  grid-cols-2 md:grid "> 
+<section className="w-10/12 m-auto place-content-center mt-32 items-center gap-8 auto-cols-fr auto-rows-min  grid-cols-2 md:grid "> 
 <figure>
    <GatsbyImage image={data.datoCmsHomepage.imageSiriusSchool.gatsbyImageData}
      />
 
     </figure>
     <article>
+    <h1 id="school" className=" text-5xl text-siriusblue font-bold  mb-10">Sirius School</h1>
+
     <div dangerouslySetInnerHTML={{ __html: data.datoCmsHomepage.texteSiriusSchool }}></div>
  
     </article>
 </section>
-<h2 className="mt-20 text-4xl text-center text-siriusblue font-bold mb-12" id="formations">Les formations </h2>
-<figure className="w-10/12 m-auto">
+<section className="w-10/12 m-auto">
+<h2 className="mt-32 text-5xl text-siriusblue font-bold mb-5" id="formations">Les formations </h2>
+<h3 className=" text-3xl text-siriusblue font-bold mb-5" >Ligne du temps </h3>
+<figure>
+
 <GatsbyImage image={data.datoCmsHomepage.ligneDuTemps.gatsbyImageData} />
 
     </figure>
-    <section className="mt-20 w-10/12 m-auto place-content-center items-center auto-cols-fr auto-rows-min gap-y-20 gap-x-20 auto-cols-fr auto-rows-min  grid-cols-2 md:grid "> 
+    </section>
+
+    <section className="mt-20 w-10/12 m-auto place-content-center items-center  auto-rows-min gap-y-20 gap-x-20 auto-cols-fr auto-rows-min  grid-cols-2 md:grid "> 
     <figure>
     <GatsbyImage image={data.datoCmsFormationUne.imageFormation.gatsbyImageData}/>
     </figure>
-<article className="bg-formationun p-10">
+<article className="bg-formationun h-full p-10">
   <h3 className="mb-5   text-2xl font-bold  text-white" >{data.datoCmsFormationUne.titreFormation} </h3>
   <div className="text-white" dangerouslySetInnerHTML={{ __html: data.datoCmsFormationUne.resumHomepage }}></div>
   <Link to="/entreprenariat-numerique">  
 
-      <button className=" p-2 px-4      bg-white font-bold rounded text-formationun mt-5"> en savoir plus</button>
+      <button className=" p-2 px-4 hover:text-opacity-70 bg-white font-bold rounded text-formationun mt-5"> en savoir plus</button>
       </Link>
 
 </article>
-<article className="bg-formationdeux p-10">
+<article className="bg-formationdeux h-full p-10">
 
 <h3 className="mb-5  text-2xl font-bold  text-white" >{data.datoCmsFormationDeux.titreFormation} </h3>
 <div className="text-white" dangerouslySetInnerHTML={{ __html: data.datoCmsFormationDeux.resumHomepage }}></div>
 <Link to="/communication-visuelle ">  
 
-<button className=" p-2 px-4 bg-white font-bold rounded text-formationdeux mt-5"> en savoir plus</button>
+<button className=" p-2 px-4 bg-white hover:text-opacity-70 font-bold rounded text-formationdeux mt-5"> en savoir plus</button>
 </Link>
 </article>
 <figure>
@@ -61,22 +67,22 @@ const IndexPage = ({ data }) => (
     <GatsbyImage image={data.datoCmsFormationTroi.imageFormation.gatsbyImageData}/>
 
     </figure>
-    <article className="bg-formationtrois p-10">
+    <article className="bg-formationtrois h-full p-10">
 
 <h3 className="mb-5   text-2xl font-bold  text-white" > {data.datoCmsFormationTroi.titreFormation}  </h3>
 <div className="text-white" dangerouslySetInnerHTML={{ __html: data.datoCmsFormationTroi.resumHomepage }}></div>
 
-      <button className=" p-2 px-4 bg-white font-bold rounded text-formationtrois mt-5"> en savoir plus</button>
+      <button className=" p-2 px-4 bg-white font-bold rounded hover:text-opacity-70 text-formationtrois"> en savoir plus</button>
 </article>
 
- <article className="bg-formationquatre p-10">
+ <article className="bg-formationquatre h-full p-10">
 
 
 <h3 className="mb-5  text-2xl font-bold  text-white" > {data.datoCmsFormationQuatre.titreFormation} </h3>
 <div className="text-white" dangerouslySetInnerHTML={{ __html: data.datoCmsFormationQuatre.resumHomepage }}></div>
 
       
-      <button className="p-2 px-4 bg-white font-bold rounded text-formationquatre mt-5"> en savoir plus</button>
+      <button className="p-2 px-4 bg-white font-bold rounded hover:text-opacity-70 text-formationquatre mt-5"> en savoir plus</button>
 </article>
 <figure>
 <GatsbyImage image={data.datoCmsFormationQuatre.imageFormation.gatsbyImageData}/>
@@ -84,7 +90,7 @@ const IndexPage = ({ data }) => (
     </figure> 
     </section>
 
-    <h2 className="mt-20 text-center text-siriusblue font-bold text-4xl  mb-12" id="témoignages"> ils sont passé par chez nous  </h2>
+    <h2 className="mt-32 text-center text-siriusblue font-bold text-4xl  mb-12" id="témoignages"> ils sont passé par chez nous  </h2>
 
      <section className=" w-10/12 m-auto md:grid grid-cols-3 items-start gap-x-20 mb-20" >
        <article className="flex flex-col align-center">
@@ -124,40 +130,42 @@ const IndexPage = ({ data }) => (
 
        </section>  
 
-<section className="bg-forms py-20" id="inscription">
+<section className="bg-forms py-32" id="inscription">
   <div className="w-10/12 m-auto md:grid grid-cols-2 ">
     <article></article>
   <article className="bg-inscription p-10 " >
   <h2 className=" text-2xl text-4xl text-siriusblue mb-5"> inscription à l'une de nos formations</h2>
   <div dangerouslySetInnerHTML={{ __html: data.datoCmsHomepage.texteInscription }}></div>
 
-    <button className="bg-siriusblue text-white text-bold mt-5 p-2 rounded"> Formulaire d'inscription</button>
+    <button className="bg-siriusorange text-white text-bold hover:opacity-90 mt-6 p-2 rounded"> Formulaire d'inscription</button>
     </article>
     </div>
 </section>
-<h2 className="mt-20 text-4xl text-gray-600 text-center mb-12"> contact</h2>
-<section className="w-6/12 m-auto" id="contact">
+<h2 className="mt-32 w-10/12 m-auto text-5xl text-siriusblue font-bold mb-5 "id="contact" >contact</h2>
+<section className="w-10/12 m-auto place-content-center items-center mt-8  gap-24 auto-cols-fr auto-rows-min  grid-cols-2 md:grid "> 
 
 <form>
 <div className="flex space-x-20">
-  <label className="flex space-x-2">
+  <label className="">
     <p>Nom</p>
     <input type="text" class="border-2 "></input>
     </label>
-    <label className="flex space-x-4">
+    <label className="">
     <p>mail</p>
-    <input type="mail" class="border-2 "></input>
+    <input type="mail"  size="30" class="border-2" ></input>
     </label>
     </div>
 <div className="flex flex-col">
-    <textarea rows="5" cols="70" className="w-12/12 mt-10 border-2" 
+    <textarea rows="5" className=" mt-10 border-2 p-2" 
     name="message" id="message" placeholder="votre message" required="">
 
     </textarea>
-      <input className="border-2 p-2 w-2/12 mt-5" type="submit" value="envoyer" />
+      <input className="bg-siriusorange item-center w-1/6 text-white text-bold mt-6 p-2 hover:opacity-70 rounded" type="submit" value="envoyer" />
       </div>
 </form>
-
+<arcticle>
+<div className="text-siriusblue " dangerouslySetInnerHTML={{ __html: data.datoCmsHomepage.texteContact }}></div>
+</arcticle>
 
 
 </section>
@@ -167,6 +175,7 @@ export const query = graphql`
   {
     datoCmsHomepage {
       texteInscription
+      texteContact
       imageSlider {
         gatsbyImageData
         (width:3000
