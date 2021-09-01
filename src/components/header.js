1976f4f0587cd62f4bf6c01975be7749004ab38a
anthow <anthonyenglebert@gmail.com>
2021-09-01 08:48:16 +0200

    /

--- src/components/header.js
+++ src/components/header.js
@@ -36,19 +36,36 @@ return(
   <GatsbyImage image={data.datoCmsLogo.logoSirius.gatsbyImageData} />  </figure> 
     <nav>
       <ul className="md:flex  text-white md:space-x-4 ">
-      <li><Link to="/#school"  className=" text-siriusblue hover:text-red-500">Sirius School</Link></li>
-      <li><Link to="/#formations"  className=" text-siriusblue hover:text-red-500">Les formations</Link></li>
-      <li><Link to="/#témoignages"  className=" text-siriusblue hover:text-red-500"> Témoignages</Link></li>
-      <li><Link to="/#inscription"  className=" text-siriusblue hover:text-red-500"> Inscription</Link></li>
-      <li><Link to="/#contact"  className=" text-siriusblue hover:text-red-500"> Contact</Link></li>
+      <li><Link to="/#school"  className=" text-siriusorange hover:text-opacity-70">Sirius School</Link></li>
+      <li><Link to="/#formations"  className=" hidden md:block text-siriusorange hover:text-opacity-70">Les formations</Link></li>
+      <li className="md:hidden">
+      <button type="button" className=" text-siriusorange hover:text-opacity-70 inline-flex md:hidden justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" 
+      id="menu-button" aria-expanded="true" aria-haspopup="true">
+      Formations
+      <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>
+        </button>
+        <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class="py-1" role="none">
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+
+    </div>
+    </div>
+        </li>  
+      <li><Link to="/#témoignages"  className=" text-siriusorange hover:text-opacity-70"> Témoignages</Link></li>
+      <li><Link to="/#inscription"  className=" text-siriusorange hover:text-opacity-70"> Inscription</Link></li>
+      <li><Link to="/#contact"  className=" text-siriusorange hover:text-opacity-70"> Contact</Link></li>
 
 
       </ul>
     </nav>
     <nav>
       <ul className="md:flex md:space-x-4 mt-10 md:mt-0 ">
-      <li><a src="https://siriushub.be" target="_blank"  className=" text-siriusorange hover:text-red-500">Sirius Hub</a></li>
-      <li><a src="https://www.possibles.org/" target="_blank"  className=" text-siriusorange hover:text-red-500">MDP</a></li>
+      <li><a src="https://siriushub.be" target="_blank"  className=" text-siriusblue hover:text-opacity-70">Sirius Hub</a></li>
+      <li><a src="https://www.possibles.org/" target="_blank"  className=" text-siriusblue hover:text-opacity-70">MDP</a></li>
       </ul>
     </nav>
     </section>
@@ -59,3 +76,4 @@ return(
 
 
 export default Header
+                
