7c1f0b566fb3fec27b45389a0191067ab0267458
anthow <anthonyenglebert@gmail.com>
2021-09-15 14:57:21 +0200

    /

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,33 +3,52 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Footer = () => (
   <footer className=" w-12/12 mt-20 pt-8 bg-white text-siriusblue">
-<section className="w-10/12 m-auto items-center flex flex-col md:flex-row justify-evenly  " >
-    <article>
-      <h4> Sirius School</h4>
-      <p> Avenue Rogier 19</p>
-      <p> 4000 Liège BE</p>
-      <p>+32 4377 02 28</p>
-      <p> info@siriushub.be</p>
-      <div className="flex mt-4">
-      </div>
-    </article>
-    <StaticImage
-      src="../images/dbsf.png"
-      alt="A dinosaur"
-      placeholder="blurred"
-      layout="fixed"
-      width={100}
-      height={75}
-    />
+    <section className="w-10/12 m-auto items-center flex flex-col md:flex-row justify-evenly  " >
+      <article>
+        <h4> Sirius School</h4>
+        <p> Avenue Rogier 19</p>
+        <p> 4000 Liège BE</p>
+        <p>+32 4377 02 28</p>
+        <p> info@siriushub.be</p>
+        <div className="flex mt-4 justify-around">
+          <a href="https://www.facebook.com/siriushub" target="_blank"> <StaticImage src="../images/facebook.png" alt=" bouton facebook"
+            width={30}
+            height={30} />
+          </a>
 
-<StaticImage
-      src="../images/MdP.png"
-      alt="A dinosaur"
-      placeholder="blurred"
-      layout="fixed"
-      width={100}
-      height={50}
-    />
+          <a href="https://www.instagram.com/sirius.hub/" target="_blank"> <StaticImage src="../images/instagram.png" alt=" bouton instagram"
+            width={30}
+            height={30} />
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCHmnuhdDfdDtb1YT42mTRUg" target="_blank"> <StaticImage src="../images/youtube.png" alt=" bouton youtube"
+            width={30}
+            height={30} />
+          </a>
+
+     
+
+        </div>
+        <div className="flex mt-4">
+        </div>
+      </article>
+      <StaticImage
+        src="../images/dbsf.png"
+        alt="A dinosaur"
+        placeholder="blurred"
+        layout="fixed"
+        width={150}
+        height={125}
+      />
+
+      <StaticImage
+        src="../images/MdP.png"
+        alt="A dinosaur"
+        placeholder="blurred"
+        layout="fixed"
+        width={150}
+        height={70}
+      />
 
 
 
