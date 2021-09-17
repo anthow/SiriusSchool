81f539928bba8f64454a333e6fb1b3ff6443f741
anthow <anthonyenglebert@gmail.com>
2021-09-17 09:07:28 +0200

    _header

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-remark-responsive-iframe`,
     `gatsby-plugin-minify`,
-    `gatsby-plugin-netlify`,
+    ` gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
